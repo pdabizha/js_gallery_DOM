0b0861ef064be07517aa74d230efb2e74cf7b9f4
Polina Dabizha <polinkadabi29@gmail.com>
2024-12-07 12:11:06 +0200

    make build

---
+++ index.12698cea.js
@@ -0,0 +1,2 @@
+document.getElementById("thumbs").addEventListener("click",function(e){var t=e.target.closest("a");t&&(document.getElementById("largeImg").src=t.href,e.preventDefault())});
+//# sourceMappingURL=index.12698cea.js.map
